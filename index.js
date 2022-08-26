97eba35df26a01e30daaba82a71628b3588b8728
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-26 20:07:05 +0600

    local  node modules example

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+// local modules
 const {add, coderName, multiply} = require('./other');
 
 const result = add(50,55);
 const result2 = multiply(5,25);
 console.log(result);
-
 console.log(coderName);
 console.log(result2);
