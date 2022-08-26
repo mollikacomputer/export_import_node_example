dfe38d3787526ae8a13576374459f856bedca0d0
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-26 19:51:57 +0600

    how can export code or function from another file for nodejs

---
+++ index.js
@@ -0,0 +1,8 @@
+const {add, coderName, multiply} = require('./other');
+
+const result = add(50,55);
+const result2 = multiply(5,25);
+console.log(result);
+
+console.log(coderName);
+console.log(result2);
---
+++ other.js
@@ -0,0 +1,10 @@
+const coderName = 'Ranjit Kumar Mandal';
+const add = (num1, num2) =>{
+    return num1 + num2;
+};
+
+const multiply = (num1, num2) => {
+    return num1*num2;
+}
+
+module.exports = {add, coderName, multiply}
